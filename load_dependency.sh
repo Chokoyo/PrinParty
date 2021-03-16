08ace08331a8d58f4374269938d5714dac583b0a
Chokoyo <40918450+Chokoyo@users.noreply.github.com>
2021-03-17 01:41:11 +0800

    Add load_dependency.sh to the repository

---
+++ load_dependency.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+currentDir="`pwd`"
+fileName="main"
+cd ncurses
+chmod 700 configure
+./configure --prefix=$currentDir --enable-widec --with-pthread --silent
+make -j --silent
+make -j --silent install
+echo "Dependencies successfully loaded!"
+g++ -I ./include -I ./include/ncursestw -L ./lib -o $fileName  $fileName.cpp -lncursestw -ldl -pthread\
+chmod 700 $fileName
+clear
+./$fileName
