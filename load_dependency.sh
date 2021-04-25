ae05da95e71cc24ca691dc4ad953a17306075168
Chokoyo <40918450+Chokoyo@users.noreply.github.com>
2021-04-26 05:41:01 +0800

    Update load_dependency.sh

--- load_dependency.sh
+++ load_dependency.sh
@@ -1,14 +1,17 @@
-#!/bin/bash
-
 currentDir="`pwd`"
 fileName="main"
 cd ncurses
 chmod 700 configure
-./configure --prefix=$currentDir --enable-widec --with-pthread --silent
-make -j --silent
-make -j --silent install
+./configure --prefix=$currentDir --enable-widec --with-pthread
+make -j
+make -j install
 echo "Dependencies successfully loaded!"
-g++ -I ./include -I ./include/ncursestw -L ./lib -o $fileName  $fileName.cpp -lncursestw -ldl -pthread
+cd ..
+g++ -I $currentDir/include -I $currentDir/include/ncursestw -L $currentDir/lib -o $fileName  $fileName.cpp cursor.cpp -lncursestw -ldl -pthread
+g++ -I $currentDir/include -I $currentDir/include/ncursestw -L $currentDir/lib -o area  area.cpp cursor.cpp -lncursestw -ldl -pthread
+g++ -I $currentDir/include -I $currentDir/include/ncursestw -L $currentDir/lib -o pong  pong.cpp cursor.cpp -lncursestw -ldl -pthread
+g++ -I $currentDir/include -I $currentDir/include/ncursestw -L $currentDir/lib -o candy  candy.cpp cursor.cpp -lncursestw -ldl -pthread
+g++ -I $currentDir/include -I $currentDir/include/ncursestw -L $currentDir/lib -o maze  maze.cpp cursor.cpp -lncursestw -ldl -pthread
 chmod 700 $fileName
 clear
 ./$fileName
