ffca574abe76e7b97e9360c34492802e1c184ac3
Chokoyo <40918450+Chokoyo@users.noreply.github.com>
2021-04-26 20:20:58 +0800

    Update load_dependency.sh

--- load_dependency.sh
+++ load_dependency.sh
@@ -7,7 +7,7 @@ make -j
 make -j install
 echo "Dependencies successfully loaded!"
 cd ..
-g++ -I $currentDir/include -I $currentDir/include/ncursestw -L $currentDir/lib -o $fileName  $fileName.cpp cursor.cpp -lncursestw -ldl -pthread
+g++ -I $currentDir/include -I $currentDir/include/ncursestw -L $currentDir/lib -o $fileName  $fileName.cpp -lncursestw -ldl -pthread
 g++ -I $currentDir/include -I $currentDir/include/ncursestw -L $currentDir/lib -o area  area.cpp cursor.cpp -lncursestw -ldl -pthread
 g++ -I $currentDir/include -I $currentDir/include/ncursestw -L $currentDir/lib -o pong  pong.cpp cursor.cpp -lncursestw -ldl -pthread
 g++ -I $currentDir/include -I $currentDir/include/ncursestw -L $currentDir/lib -o candy  candy.cpp cursor.cpp -lncursestw -ldl -pthread
