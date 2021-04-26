56d506f8b89d1cf5e365d6f4eb78195ddf5c3915
Chokoyo <40918450+Chokoyo@users.noreply.github.com>
2021-04-26 19:41:36 +0800

    Update pong.cpp

--- pong.cpp
+++ pong.cpp
@@ -1,24 +1,33 @@
+// Author: Gu Zhuangcheng
+// U. No.: 3035827110
+// ENGG1340 Group Project
+// File Name: pong.cpp
+// Description: The is a ping pong game implemented by multi-thread programming
+
 #include <ncurses.h>
-#include <ctime>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <locale.h>
 #include <vector>
 #include <algorithm>
-#include <fstream>
 #include <chrono>
 #include <unistd.h>
 #include <thread>
-#include <iostream>
 using namespace std;
 
+// global variables
 int board[15][60] = {}, scores[] = {0, 0};
 int xDirect[] = {-2, 2}, yDirect[] = {-1, 1};
 bool tThreadEnd = 0, ballThreadEnd = 0;
 
+// function prototypes
 int random(int x);
+void printScore(WINDOW *dwin, int playerNum);
+void printSeperator(WINDOW *dwin, int playerNum);
+void printBuffle(WINDOW *dwin, int y, int x, bool clear);
 
+// structure for the ball
 struct ball
 {
     string symbol = "●";
@@ -38,14 +47,16 @@ struct ball
     };
 } myBall;
 
+// generate a random number range from 0 to x-1
 int random(int x)
 {
     return (rand() % x);
 }
 
+// print the score for the player(s)
 void printScore(WINDOW *dwin, int playerNum)
 {
-    mvwprintw(dwin, 15, 31, "SCORE");
+    mvwprintw(dwin, 15, 31, "Score");
     for (int i = 0; i < playerNum; i++)
     {
         mvwprintw(dwin, 16, 38 + i * 10, "        ");
@@ -55,23 +66,26 @@ void printScore(WINDOW *dwin, int playerNum)
     wrefresh(dwin);
 }
 
+// moveBall thread
 void moveBall(WINDOW *dwin, int playerNum)
 {
     int detect = 0;
     while (1)
     {
+        // control to stop and run of the thread
         if (tThreadEnd == 1)
         {
             ballThreadEnd == 1;
             break;
         }
+        // initialize th ball position
         myBall.position = {7, 29};
         myBall.direction = {yDirect[random(2)], xDirect[random(2)]};
         this_thread::sleep_for(chrono::milliseconds(997));
         while (1)
         {
             detect = board[myBall.nextPos()[0]][myBall.nextPos()[1]];
-            // mvwinstr(win, myBall.nextPos()[0], myBall.nextPos()[1], c);
+            // detect whether ball in not catch
             if (myBall.position[1] <= 3)
             {
                 mvwaddstr(dwin, myBall.position[0], myBall.position[1], " ");
@@ -100,6 +114,8 @@ void moveBall(WINDOW *dwin, int playerNum)
             {
                 myBall.direction[0] = 0 - myBall.direction[0];
             }
+
+            // move the ball to the next position
             mvwaddstr(dwin, myBall.position[0], myBall.position[1], " ");
             myBall.move();
             mvwaddstr(dwin, myBall.position[0], myBall.position[1], "●");
@@ -109,19 +125,23 @@ void moveBall(WINDOW *dwin, int playerNum)
     }
 }
 
+// timer thread
 void timer(WINDOW *dwin)
 {
     mvwprintw(dwin, 15, 2, "TIME");
+    // this thread will run independently
     for (int i = 30; i >= 0; i--)
     {
         mvwprintw(dwin, 16, 5, "          ");
         mvwprintw(dwin, 16, 5, (to_string(i) + "s").c_str());
         wrefresh(dwin);
+        // delay the thread for 1 sec in each iteration
         this_thread::sleep_for(chrono::milliseconds(997));
     }
     tThreadEnd = 1;
 }
 
+// print the seperator
 void printSeperator(WINDOW *dwin, int playerNum)
 {
     for (int i = 1; i < 59; i++)
@@ -135,6 +155,7 @@ void printSeperator(WINDOW *dwin, int playerNum)
     wrefresh(dwin);
 }
 
+// print or clear the player's buffle
 void printBuffle(WINDOW *dwin, int y, int x, bool clear)
 {
     for (int i = 0; i < 5; i++)
@@ -154,26 +175,25 @@ void printBuffle(WINDOW *dwin, int y, int x, bool clear)
     }
 }
 
+// the pong game function
 void pong(int round, vector<int> score, int numOfPlayer)
 {
+    // initialize ncurses
     setlocale(LC_ALL, "");
     initscr();
     noecho();
     raw();
     curs_set(0);
+    // initialize the random seed
     srand(time(NULL));
 
+    // initialize variable
     WINDOW *win = newwin(20, 60, 0, 0);
     keypad(win, true);
     if (numOfPlayer == 1)
         scores[0] += 10;
     int input, p1 = 5, p2 = 5;
     vector<thread> timeThread(3), ballThread(3);
-    // for (int i = 0; i < 3; i++)
-    // {
-    //     timeThread.push_back(thread(timer, win));
-    //     ballThread.push_back(thread(moveBall, win));
-    // }
 
     // fill the board
     for (int i = 0; i < 60; i++)
@@ -186,18 +206,20 @@ void pong(int round, vector<int> score, int numOfPlayer)
         board[i][0] = -1;
         board[i][59] = -1;
     }
+
+    // print the basic information
     box(win, 0, 0);
     printSeperator(win, numOfPlayer);
     mvwprintw(win, 0, 3, "[GAME: Pong]");
-    mvwprintw(win, 19, 8, "[round 1/2]");
-    mvwprintw(win, 15, 2, "TIME");
+    mvwprintw(win, 19, 8, "[ROUND 1/2]");
+    mvwprintw(win, 15, 2, "Time");
     printScore(win, numOfPlayer);
     mvwprintw(win, 7, 19, "Press Any Key to Start");
     wrefresh(win);
     wgetch(win);
     mvwprintw(win, 7, 22, "                      ");
 
-    for (int i = 1; i <= 2; i++) // totally 3 rounds
+    for (int i = 1; i <= 2; i++) // totally 2 rounds
     {
         tThreadEnd = 0;
         ballThreadEnd = 0;
@@ -213,6 +235,8 @@ void pong(int round, vector<int> score, int numOfPlayer)
         wrefresh(win);
         timeThread[i - 1] = thread(timer, win);
         ballThread[i - 1] = thread(moveBall, win, numOfPlayer);
+
+        // start the thread
         timeThread[i - 1].detach();
         ballThread[i - 1].detach();
         while (1)
@@ -287,6 +311,6 @@ void pong(int round, vector<int> score, int numOfPlayer)
 int main(int argc, char *argv[])
 {
     string score1 = argv[1], score2 = argv[2], pNum = argv[3];
-    pong(0, {stoi(score1), stoi(score2)}, stoi(pNum));
+    pong(0, {stoi(score1), stoi(score2)}, stoi(pNum)); /*pong(round,score[],playerNum})*/
     return 0;
 }
